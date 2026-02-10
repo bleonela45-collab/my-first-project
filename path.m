711fcb83fd1926e6979d42828ce65258f3f50d78
Leonela Baez <bleonela45@gmail.com>
2026-02-10 11:43:48 -0300

    Crear path.m

---
+++ path.m
@@ -0,0 +1,29 @@
+# my-first-project
+#variables
+project = "first"
+
+print ("the is: ", project)
+
+#Tendriamos que agregar proyectosss y denas
+<head>
+
+#probar crear una pag blog, para 17 de objetivoss y denass
+
+#podriamoss usar y crear un nuevo archivoo
+#la verdad ya no se que poner o que armar
+
+va a doler
+
+#Siempre va a doler.
+
+vivimos en un mundo donde tienen normalizado el dolor y que NO esta bien, expresarse mediante llanto, protesta y demas
+
+#la vida del obrero es asi
+
+#¿Porque la vida del obrero es asi?
+
+metafora usada en una cancion vieja, llamada homero. es la cancion
+
+si tu te vas y yo me voy, que dios nos libre de amar a quien no nos ama
+
+#Y no se que pasa, la vida sorprende y a veces solo avanza. te juro
